c08ce994ee07f5a4a5195bb66ee2f34d3591bdcd
mingcs <54163614+mingcs@users.noreply.github.com>
2020-06-18 12:10:37 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
 connectDB({ useNewUrlParser: true });
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
